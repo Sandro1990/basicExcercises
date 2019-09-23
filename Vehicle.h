d76a0b4cb7801bda5b059834aa03485b042cc22f
Sandro <sandro.kuveljic@gmail.com>
2019-09-23 16:10:04 +0200

    memory leak

---
+++ Vehicle.h
@@ -0,0 +1,21 @@
+#pragma once
+class Vehicle
+{
+public:
+	Vehicle();
+	~Vehicle();
+	
+	int GetNumberOfWheels();
+
+
+private:	
+	int _wheels;
+
+	// Code conventions
+	// cameCase
+	// PascalCase
+	// snake_case
+	// SCREAMING_SNAKE_CASE
+	// kebab-case
+};
+
